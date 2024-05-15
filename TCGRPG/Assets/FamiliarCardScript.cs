66db0fb289ac2ccd57932ab67e6a1fdfb3bd5592
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-05-15 08:49:30 -0400

    Added Some Comments

--- TCGRPG/Assets/FamiliarCardScript.cs
+++ TCGRPG/Assets/FamiliarCardScript.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class FamiliarCardScript : MonoBehaviour
 {
     [SerializeField] FamiliarCardData cardData;
-    [HideInInspector] public List<GameObject> targets = new List<GameObject>(); //Constantly changing variable to tell the card what to target for abilities
-    
+    public List<GameObject> targets = new List<GameObject>(); //Constantly changing variable to tell the card what to target for abilities
+    [SerializeField] public List<object> statusEffects = new List<object>(); //List of status effects, evens (including 0) are the effects, the following odds are the numbers associated with the effects
 }
