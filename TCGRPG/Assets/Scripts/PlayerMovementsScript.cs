fbf0dba265312087e1b4ad0ba5a76107d72867e3
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-05-18 14:55:08 -0400

    Scraped some old 2D movement code

--- TCGRPG/Assets/Scripts/PlayerMovementsScript.cs
+++ TCGRPG/Assets/Scripts/PlayerMovementsScript.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMovementsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    private Vector2 targetMovement;
+    private Vector2 currentMovement;
+
+    // public Collider3D playerCollider;
+
+    [Header("Movement Variables")]
+    public float moveSpeed;
+    public float accelleration;
     void Update()
     {
-        
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        targetMovement = moveInput * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+        currentMovement = Vector2.MoveTowards(currentMovement, targetMovement, accelleration * Time.fixedDeltaTime);
+
+        rb.velocity = currentMovement;
+    }
+
+    public void Shove(Vector2 force)
+    {
+        currentMovement += force;
     }
 }
